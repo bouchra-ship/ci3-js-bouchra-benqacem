27727a6653c10f730dfbb9e42bbf887df98f56ba
bouchra benqacem <elwidad1937@gmail.com>
2025-04-16 23:29:00 +0200

    une fonction  qui prend en paramètre une chaîne de caractères et renverse l'ordre des mot.

---
+++ lv2-ex4.js
@@ -0,0 +1,13 @@
+function renverserChaine(chaine) {
+  let mots = chaine.split(" ");
+  let motsRenverses = mots.reverse();
+  return motsRenverses.join(" ");
+}
+function renverserChaine(chaine) {
+  let mots = chaine.split(" ");
+  let motsRenverses = mots.reverse();
+  return motsRenverses.join(" ");
+}
+
+let resultat = renverserChaine("Bonjour le monde");
+console.log(resultat);
